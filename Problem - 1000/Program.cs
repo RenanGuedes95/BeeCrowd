5991d6336d38ef59e28a9b44fb12c53b7aeca25d
RenanGuedes95 <renan.brega@viannasempre.com.br>
2025-04-01 00:18:12 -0300

    Resolução do problema código 1000 em C#.

---
+++ Problem - 1000/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+
+Data de resolução: 01/04/2025.
+Código do problema: 1000
+
+Bem-vindo ao beecrowd!
+
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+Entrada
+Este problema não possui nenhuma entrada.
+
+Saída
+Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
+
+
+*/
+
+
+using System;
+
+public class Program{
+    public static void Main(string[] args){
+        Console.WriteLine("Hello World!");
+    }
+}
