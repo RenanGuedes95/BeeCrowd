e5933935fdb7428ddf4cbf8fdf79eb27e2e2fe47
RenanGuedes95 <renan.brega@viannasempre.com.br>
2025-04-03 00:20:29 -0300

    Resolução do problema 1004 em C#.

---
+++ Problem - 1004/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Data de resolução: 02/04/2025.
+Código do problema: 1004
+
+Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.   
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros.
+
+Saída
+Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.
+
+*/
+
+using System;
+
+class URI{
+    public static int multiplicaInteiros(int A, int B){
+        return A * B;
+    }
+    public static void Main(string[] args){
+        int A = Convert.ToInt32(Console.ReadLine());
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        int PROD = multiplicaInteiros(A, B);
+
+        Console.WriteLine($"PROD = {PROD}");
+
+    }
+}
